d4605c7347a5a5305aa33c96c5a0f4feb5f33b19
Diogo Ribeiro <diogo_dj@hotmail.com>
2025-11-12 12:03:11 +0000

    Add module docstring and version management
    Added module docstring and version information.

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,21 @@
+"""
+PSOD: Pseudo-Supervised Outlier Detection
 
+A flexible outlier detection library using pseudo-supervised learning approach.
+"""
+
+# TODO: Add proper version management (consider using versioneer or setuptools-scm)
+__version__ = "0.1.0"
+
+# TODO: Import main classes and functions for easier access
+# from .core import PSOD
+# from .utils import load_model, save_model
+# from .visualization import plot_outlier_scores
+
+# TODO: Define __all__ for explicit API
+__all__ = [
+    # "PSOD",
+    # "load_model",
+    # "save_model",
+    # "plot_outlier_scores",
+]
